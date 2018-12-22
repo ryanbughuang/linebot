b7e05d863c52338276e9dae11a733f8945757d65
ryanhuang <ryanhuang970623@gmail.com>
2018-12-22 20:41:04 +0800

    change color

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, template_message) # 送出訊息，訊息內容為'template_message'
     elif text == '吃吃':
         carousel_template = CarouselTemplate(imageAspectRatio='rectangle',imageSize='cover',columns=[
-            CarouselColumn(thumbnailImageUrl = 'https://i.imgur.com/wT9Rjq7t.jpg',text='大門', actions=[
+            CarouselColumn(imageBackgroundColor='#CFCFEA',text='大門', actions=[
                 MessageAction(label='飯', text='大門_飯'),
                 MessageAction(label='麵', text='大門_麵'),
                 MessageAction(label='其他', text='大門_其他')
