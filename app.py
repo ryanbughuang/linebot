496cf9de77ca0bfd4ba5b0698a0ccc365e20e4bc
ryanhuang <ryanhuang970623@gmail.com>
2018-12-22 21:27:51 +0800

    change size

--- app.py
+++ app.py
@@ -55,22 +55,22 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, template_message) # 送出訊息，訊息內容為'template_message'
     elif text == '吃吃':
         carousel_template = CarouselTemplate(imageAspectRatio='rectangle',imageSize='contain',columns=[
-            CarouselColumn(text='大門',thumbnailImageUrl = 'https://i.imgur.com/7cdcpMC.jpg', actions=[
+            CarouselColumn(text='大門',thumbnailImageUrl = 'https://i.imgur.com/fIKfTIi.jpg', actions=[
                 MessageAction(label='飯', text='大門_飯'),
                 MessageAction(label='麵', text='大門_麵'),
                 MessageAction(label='其他', text='大門_其他')
             ]),
-            CarouselColumn(text='公館',thumbnailImageUrl = 'https://i.imgur.com/7cdcpMC.jpg', actions=[
+            CarouselColumn(text='公館',thumbnailImageUrl = 'https://i.imgur.com/fIKfTIi.jpg', actions=[
                 MessageAction(label='飯', text='公館_飯'),
                 MessageAction(label='麵', text='公館_麵'),
                 MessageAction(label='其他', text='公館_其他')
             ]),
-            CarouselColumn(text='溫州',thumbnailImageUrl = 'https://i.imgur.com/7cdcpMC.jpg', actions=[
+            CarouselColumn(text='溫州',thumbnailImageUrl = 'https://i.imgur.com/fIKfTIi.jpg', actions=[
                 MessageAction(label='飯', text='溫州_飯'),
                 MessageAction(label='麵', text='溫州_麵'),
                 MessageAction(label='其他', text='溫州_其他')
             ]),
-            CarouselColumn(text='後門',thumbnailImageUrl = 'https://i.imgur.com/7cdcpMC.jpg', actions=[
+            CarouselColumn(text='後門',thumbnailImageUrl = 'https://i.imgur.com/fIKfTIi.jpg', actions=[
                 MessageAction(label='飯', text='後門_飯'),
                 MessageAction(label='麵', text='後門_麵'),
                 MessageAction(label='其他', text='後門_其他')
