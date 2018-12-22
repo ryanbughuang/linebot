90f0b00ee6f4bb3113315cfa819c421592f394cf
ryanhuang <ryanhuang970623@gmail.com>
2018-12-22 20:02:48 +0800

    thumbnail image test

--- app.py
+++ app.py
@@ -53,24 +53,24 @@ def handle_message(event):
         template_message = TemplateSendMessage(
             alt_text='Buttons alt text', template=buttons_template)
         line_bot_api.reply_message(event.reply_token, template_message) # 送出訊息，訊息內容為'template_message'
-    if text == '吃吃':
+    elif text == '吃吃':
         carousel_template = CarouselTemplate(columns=[
-            CarouselColumn(text='大門',thumbnailImageUrl = 'https://via.placeholder.com/1024x1024', actions=[
+            CarouselColumn(text='大門',thumbnailImageUrl = 'https://photos.app.goo.gl/N7Dev9mZD98Qqbfv7', actions=[
                 MessageAction(label='飯', text='大門_飯'),
                 MessageAction(label='麵', text='大門_麵'),
                 MessageAction(label='其他', text='大門_其他')
             ]),
-            CarouselColumn(text='公館', actions=[
+            CarouselColumn(text='公館',thumbnailImageUrl = 'https://photos.app.goo.gl/N7Dev9mZD98Qqbfv7', actions=[
                 MessageAction(label='飯', text='公館_飯'),
                 MessageAction(label='麵', text='公館_麵'),
                 MessageAction(label='其他', text='公館_其他')
             ]),
-            CarouselColumn(text='溫州', actions=[
+            CarouselColumn(text='溫州',thumbnailImageUrl = 'https://photos.app.goo.gl/N7Dev9mZD98Qqbfv7', actions=[
                 MessageAction(label='飯', text='溫州_飯'),
                 MessageAction(label='麵', text='溫州_麵'),
                 MessageAction(label='其他', text='溫州_其他')
             ]),
-            CarouselColumn(text='後門', actions=[
+            CarouselColumn(text='後門',thumbnailImageUrl = 'https://photos.app.goo.gl/N7Dev9mZD98Qqbfv7', actions=[
                 MessageAction(label='飯', text='後門_飯'),
                 MessageAction(label='麵', text='後門_麵'),
                 MessageAction(label='其他', text='後門_其他')
