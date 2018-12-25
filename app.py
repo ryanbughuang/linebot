81a57d5e8a525796289d65a489402b6f4ad58ded
ting1209 <34576020+ting1209@users.noreply.github.com>
2018-12-25 18:49:09 +0800

    恢復成沒push

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
 from __future__ import unicode_literals
-<<<<<<< HEAD
-=======
-import schedule
-import time
->>>>>>> 02dc0233301967d852bdb4eff05bb7621c3ae0fa
 import pandas as pd
 import numpy as np
 import errno
@@ -23,20 +18,6 @@ from linebot.models import *
 
 app = Flask(__name__)
 
-
-<<<<<<< HEAD
-=======
-##push 
-def noti(message):
-    line_bot_api.push_message('Ubd3667a82df0a6c42366c6d3fa104def', TextSendMessage(text =message))
-    return True	
-	
-schedule.every().day.at("17:50").do(noti('來找找今天吃什麼鴨^^'))
-	
-while True:
-    schedule.run_pending()
-
->>>>>>> 02dc0233301967d852bdb4eff05bb7621c3ae0fa
 #
 all_restaurant = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRR3IygA5p4RzvLnqct1YS_5PngAP9ANKdcK0fhTuWEI6zA52YrqFyS-dBex3b6lcqt5WM4kQE0r3Oh/pub?output=csv',header=0)
 def rest_selector(reply_text):
