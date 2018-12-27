59f3ff8d2734cfa26cf166b8e2ec6d4594e7b551
huang <b04701216@ntu.edu.tw>
2018-12-27 12:07:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -97,25 +97,6 @@ def rest_con(reply_text):
                                 spacing='sm',
                                 contents=[
                                     TextComponent(
-                                        text='Place',
-                                        color='#aaaaaa',
-                                        size='sm',
-                                        flex=1
-                                    ),
-                                    TextComponent(
-                                        text=res_location[0],
-                                        wrap=True,
-                                        color='#666666',
-                                        size='sm',
-                                        flex=5
-                                    )
-                                ],
-                            ),
-                            BoxComponent(
-                                layout='baseline',
-                                spacing='sm',
-                                contents=[
-                                    TextComponent(
                                         text='Time',
                                         color='#aaaaaa',
                                         size='sm',
@@ -142,25 +123,6 @@ def rest_con(reply_text):
                                     ),
                                     TextComponent(
                                         text=res_price[0],
-                                        wrap=True,
-                                        color='#666666',
-                                        size='sm',
-                                        flex=5,
-                                    ),
-                                ],
-                            ),
-                            BoxComponent(
-                                layout='baseline',
-                                spacing='sm',
-                                contents=[
-                                    TextComponent(
-                                        text='Rate',
-                                        color='#aaaaaa',
-                                        size='sm',
-                                        flex=1
-                                    ),
-                                    TextComponent(
-                                        text=res_rate[0],
                                         wrap=True,
                                         color='#666666',
                                         size='sm',
