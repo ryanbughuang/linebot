730d932d5021d25132b740d10ae7e590c992ef81
ryanhuang <ryanhuang970623@gmail.com>
2019-01-02 17:50:09 +0800

    修改食記搜尋

--- app.py
+++ app.py
@@ -326,7 +326,7 @@ def random_res_recommand():
                     ButtonComponent(
                         style='link',
                         height='sm',
-                        action=URIAction(label='食記', uri='https://www.google.com/search?q='+res_name+'+'+res_location[0])
+                        action=URIAction(label='食記', uri='https://www.google.com/search?q='+res_name)
                     ),
                     SeparatorComponent(),
                     ButtonComponent(
